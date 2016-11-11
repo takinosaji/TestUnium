5465b6698094565d3b9d1a2d4bd832da7b660a1c
TakinosaJi <kostiantyn.chomakov@gmail.com>
2016-11-11 22:34:25 +0100

    small fixed

--- src/TestUnium/TestCore.cs
+++ src/TestUnium/TestCore.cs
@@ -1,6 +1,11 @@
-﻿using TestUnium.Settings;
+﻿using TestUnium.Sessioning;
+using TestUnium.Settings;
+using TestUnium.Stepping;
+using TestUnium.Stepping.Steps;
 
 namespace TestUnium
 {
+    [SessionContext(typeof(ContextBase))]
+    [StepRunner(typeof(StepRunnerBase))]
     public class TestCore : SettingsDrivenTest { }
 }
