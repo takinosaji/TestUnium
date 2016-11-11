2296b8c1364af9a3f5cf7f4756c1145a5167cb18
TakinosaJi <kostiantyn.chomakov@gmail.com>
2016-11-11 22:03:56 +0100

    settings fix try

--- demo/xUnitDemoProject/Tests/ContextIndependentTests/CustomSettingsTests/CustomSettings.cs
+++ demo/xUnitDemoProject/Tests/ContextIndependentTests/CustomSettingsTests/CustomSettings.cs
@@ -24,7 +24,7 @@ namespace xUnitDemoProject.Tests.ContextIndependentTests.CustomSettingsTests
         }
         public override void PostInitializeAction()
         {
-            ChromeDriverPath = "drivers";
+            ChromeDriverPath = "..\\drivers";
         }
     }
 }
